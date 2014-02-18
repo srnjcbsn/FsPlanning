ee58ff264b27b57d7aa5c9864a5f3b737cfdfe01
Søren Jacobsen <srnjcbsn@gmail.com>
2014-02-18 17:06:31 +0100

    Implemented Priority!Queue. Seems to be working.
    Implemented basic types for problem solving (Problem and SearchNode types

---
+++ FsPlanning/FsPlanning/PriorityQueue.fs
@@ -0,0 +1,46 @@
+﻿namespace FsPlanning
+    open System
+    open System.Collections.Generic
+
+    type PriorityQueue<'TState,'TPriority when 'TState : comparison and 'TPriority : comparison> 
+        (sortedSet : Set<'TPriority * 'TState>, stateToCostMap : Map<'TState, 'TPriority>) =
+        class
+            let SortedSet = sortedSet
+            let StateToCostMap = stateToCostMap
+
+            member this.Add (cost, state) =
+                new PriorityQueue<_,_> (Set.add (cost, state) SortedSet, Map.add state cost StateToCostMap)
+            
+            member this.Pop () =
+                let min = Set.minElement SortedSet
+                let newSet = Set.remove min SortedSet 
+                let newMap = Map.remove (snd min) StateToCostMap
+                (min, new PriorityQueue<_,_> (newSet, newMap))
+
+            member this.PriorityOf (state) =
+                Map.tryFind state StateToCostMap
+
+            member this.Exists (state) =
+                Set.contains state SortedSet
+
+            new (elems : seq<'TPriority * 'TState>) =
+                let SSet = Set.ofSeq elems
+                let PMap = Map.ofSeq <| Seq.map (fun (a,b) -> (b,a)) elems
+                new PriorityQueue<_,_> (SSet, PMap)
+        end
+
+    module PriorityQueue = 
+
+        let add cost state (pq : PriorityQueue<_,_>) = pq.Add (cost, state)
+
+        let pop (pq : PriorityQueue<_,_>) = pq.Pop ()
+
+        let priorityOf state (pq : PriorityQueue<_,_>) = pq.PriorityOf (state)
+
+        let exists state (pq : PriorityQueue<_,_>) = pq.Exists (state)
+
+        let ofSeq elems = new PriorityQueue<_,_> (elems)
+
+            
+   
+    
---
+++ FsPlanning/FsPlanning/Searching.fs
@@ -0,0 +1,35 @@
+﻿namespace FsPlanning
+module Searching =
+    
+    type Action<'TState> = 'TState -> 'TState * int
+
+    type Problem<'TState> =
+        { InitialState : 'TState
+        ; GoalTest : 'TState -> bool
+        ; Actions : 'TState -> Action<'TState> list
+        }
+
+    type SearchNode<'TState> =
+        { State    : 'TState
+        ; Parent   : SearchNode<'TState> option
+        ; Action   : Action<'TState>
+        ; PathCost : int
+        }
+
+    
+    let initialNode problem =
+        { State    = problem.InitialState
+        ; Parent   = None
+        ; Action   = (fun a -> a, 0)
+        ; PathCost = 0
+        }
+    
+    let childNode (node : SearchNode<'a>) action = 
+        let (childState, stepCost) = action node.State
+        { State  = childState
+        ; Parent = Some node
+        ; Action = action
+        ; PathCost = node.PathCost + stepCost
+        }
+
+    let childNodes node problem = List.map (childNode node) <| problem.Actions node.State
