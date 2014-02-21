b2de29fc5df7264dea37e18963449f16829977e1
Søren Jacobsen <srnjcbsn@gmail.com>
2014-02-21 16:57:52 +0100

    Implemented BallsInBaskets problem as a quick test, seems to be working. (No unit tests, only an ugly executable)

---
+++ FsPlanning/BallsInBaskets/BIB.fs
@@ -0,0 +1,30 @@
+﻿namespace BallsInBaskets
+module BIB =
+    
+    open FsPlanning.Searching
+
+    type Ball = int
+    type Basket = int
+    type BibState = Map<Ball, Basket>
+
+    type BibAction = Move of Ball * Basket
+
+    let actionsForBall numberOfBaskets ball basket = 
+        List.map (fun i -> Move (ball, i)) [for i in 1 .. numberOfBaskets do if i <> basket then yield i]
+
+    let actions numberOfBaskets state = 
+        List.concat (List.map (fun (ball, basket) -> actionsForBall numberOfBaskets ball basket) <| Map.toList state)
+
+    let result state (Move (ball, basket)) = Map.add ball basket state
+
+    let stepCost state action = 1
+
+    let problem numberOfBaskets initialState goalTest = 
+        { InitialState = initialState
+        ; GoalTest = goalTest
+        ; Actions = actions numberOfBaskets
+        ; Result = result
+        ; StepCost = stepCost
+        }
+
+    
---
+++ FsPlanning/BallsInBaskets/Program.fs
@@ -0,0 +1,19 @@
+﻿
+// NOTE: If warnings appear, you may need to retarget this project to .NET 4.0. Show the Solution
+// Pad, right-click on the project node, choose 'Options --> Build --> General' and change the target
+// framework to .NET 4.0 or .NET 4.5.
+
+module BallsInBaskets.Main
+
+open System
+open BIB
+open FsPlanning.Searching
+
+let someFunction x y = x + y
+
+[<EntryPoint>]
+let main args = 
+    let prob = problem 3 (Map [(1,1)]) (Map.find 1 >> ((=) 3))
+    printfn "%A" <| aStar prob
+    0
+
