c7e65f409c8699fa30e382cedf803936041c7892
Søren Jacobsen <srnjcbsn@gmail.com>
2014-03-06 15:26:27 +0100

    Corrected syntex error causing false type inference

--- FsPlanning/FsPlanning/Searching.fs
+++ FsPlanning/FsPlanning/Searching.fs
@@ -88,7 +88,7 @@ module Searching =
 
     let rec unRavelPath node = 
         match node.Parent with
-        | Some node' -> (unRavelPath node') @ node.Action.Value
+        | Some node' -> (unRavelPath node') @ [node.Action.Value]
         | None -> []
 
     let solve (solver : Problem<'s,'a> -> SearchNode<'s,'a> option) problem =
