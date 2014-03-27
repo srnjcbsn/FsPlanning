57f90144f19f0258f5e313dca0d66f763907ddfa
jannick johnsen <jannickboese@msn.com>
2014-03-27 20:55:32 +0100

    Updated Agent/BDIAgent.fs

--- FsPlanning/FsPlanning/Agent/BDIAgent.fs
+++ FsPlanning/FsPlanning/Agent/BDIAgent.fs
@@ -5,17 +5,30 @@
     open Planning
 
     [<AbstractClass>]
-    type public BDIAgent<'TPercept,'TState,'TAction,'TIntention when 'TState : comparison>(initstate,desires,planner) = class 
+    type public BDIAgent<'TPercept,'TState,'TAction,'TIntention,'TSolution when 'TState : comparison>(initstate,desires,planner) = class 
         
         let desires:DesireTree<'TState,'TIntention> = desires
-        let planner:Planner<'TState, 'TAction,'TIntention> = planner
-        let mutable intentions = []
+        let planner:Planner<'TState, 'TAction,'TIntention,'TSolution> = planner
+        let mutable intentionIdCounter = 0L
+        let mutable intentions = Map.empty
+        let mutable solutions = Map.empty
         let mutable state = initstate
         let mutable actuators = []
         let mutable sensors = []
 
+        let mutable runningSolution = None
+        let mutable solutionsOnHold = []
+
+        //locks
         let stateLock = new Object()
+        let solutionsLock = new Object()
+        let runningSolutionLock = new Object()
+        let solutionOnHoldLock = new Object()
         let intentionLock = new Object()
+        let intentionIdLock = new Object()
+
+        let generateIntentionId = lock intentionIdLock  (fun () -> intentionIdCounter <- intentionIdCounter + 1L
+                                                                   intentionIdCounter)
 
         let rec travelDesires prio state (dTree:DesireTree<_,_>) = 
             match dTree with
@@ -38,15 +51,16 @@
         
         
         let updateIntentions intenfilter currentInt (prio,intention) =
-            let (conflics,harmonic) = List.partition (fun (_,i) ->  
+            let (conflics,harmonic) = Map.partition (fun  _ (_,i) ->  
                                                     let filter = intenfilter (i,intention)
                                                     match filter with
                                                     | Conflictive -> true
                                                     | Harmonic -> false
                                                     ) currentInt
-            let highestPrio = List.forall (fun (cp,_) -> prio > cp) conflics
+            let highestPrio = Map.forall (fun _ (cp,_) -> prio > cp) conflics
             if highestPrio then
-                (prio,intention)::harmonic
+                let id = generateIntentionId
+                Map.add id (prio,intention) harmonic
             else
                 currentInt
 
@@ -62,6 +76,77 @@
             )
         
 
+        let rec executePlan state intention id plan =
+            if planner.SolutionFinished (state,intention,plan) then
+                (true, plan)
+            else
+                let (action,rest) = planner.NextAction (state,intention,plan)
+                let findActuator = List.tryFind (fun (act:Actuator<_>) -> act.CanPerformAction action ) actuators
+                match findActuator with
+                | Some actuator -> 
+                    actuator.PerformAction action
+                    executePlan state intention id rest
+                | None ->
+                    (false,plan)
+            
+
+        let attemptPromote id intention plan  =
+            lock solutionsLock (fun () -> solutions <- Map.add id plan solutions)
+            lock runningSolutionLock (fun () ->
+                runningSolution <- Some id
+                let curState = state
+                
+                let planWillWork = planner.PlanWorking (curState,intention,plan)
+                let (planFinished,remaining) =  if planWillWork then
+                                                    executePlan curState intention id plan
+                                                else
+                                                    false,plan
+                runningSolution <- None
+                if planFinished then
+                    lock intentionLock (fun () -> intentions <- Map.remove id intentions)
+                    lock solutionsLock (fun () -> solutions <- Map.remove id solutions )
+                else
+                    lock solutionsLock (fun () -> solutions <- Map.add id remaining solutions )
+                    lock solutionOnHoldLock (fun () -> solutionsOnHold <- id :: solutionsOnHold)
+
+                
+            )
+        
+
+//        let checkActuators =
+//            let curSolId = runningSolution
+//            match curSolId with
+//            | Some id -> 
+//                let trySol = Map.tryFind id solutions
+//                match trySol with
+//                | Some sol -> 
+//                    
+//                | _ -> ()
+//            | _ -> ()
+
+//        let planReady id =
+//            let startActuators = 
+//                lock runningSolutionLock    
+//                    (fun () -> 
+//                        match runningSolution with
+//                        | None -> runningSolution <- Some id
+//                                  true
+//                        | _ -> false
+//                    )
+//            if startActuators then
+//                ()
+        
+//        let asyncIntentionPlanning state (id,intention) =
+//            Async.Start 
+//                (async
+//                    {
+//                        let plan = planner.FormulatePlan (state,intention)
+//                        match plan with
+//                        | Some sol -> 
+//                            lock solutionLock (fun () -> solutions <- Map.add id plan solutions)
+//                        | _ -> ()
+//                    })
+
         member private this._newPercepts (sensor:Sensor<'TPercept>) = 
             let comp =  
                     async
@@ -69,6 +154,7 @@
                             let percepts = sensor.ReadPercepts()
                             lock stateLock (fun () ->  state <- List.fold (fun s p -> this.AnalyzePercept(p,s) ) state percepts)    
                             buildIntentions this.FilterIntention state
+
                         }
             
             Async.Start comp
--- FsPlanning/FsPlanning/Agent/Planning.fs
+++ FsPlanning/FsPlanning/Agent/Planning.fs
@@ -12,5 +12,9 @@ module Planning =
         | Desire of ('state -> 'goal option)
         | ManyDesires of DesireTree<'state,'goal> list
     
-    type Planner<'TState, 'TAction,'TGoal> =
-        abstract member FormulatePlan : ('TState*'TGoal) -> Solution<'TAction>
+    type Planner<'TState, 'TAction, 'TGoal,'TSolution> =
+        abstract member FormulatePlan : ('TState*'TGoal) -> 'TSolution option
+        abstract member PlanWorking : ('TState*'TGoal*'TSolution) -> bool
+        abstract member RepairPlan : ('TState*'TGoal*'TSolution) -> 'TSolution option
+        abstract member SolutionFinished : ('TState*'TGoal*'TSolution) -> bool
+        abstract member NextAction : ('TState*'TGoal*'TSolution) -> 'TAction*'TSolution
