25a9d87a9f165dca7dc2b606fd5b413257ddeee9
jannickj <jannickboese@msn.com>
2014-05-26 22:36:59 +0200

    Added all pairs algorithm

--- FsPlanning/FsPlanning/Search/Astar.fs
+++ FsPlanning/FsPlanning/Search/Astar.fs
@@ -66,6 +66,33 @@ module Astar =
         | Some solution -> Some <| {Path = unRavelPath solution; Cost = solution.PathCost}
         | None -> None
 
+      
+    let aStarAllPaths problem =
+        let updateFrontier explored frontier node = 
+            let inExplored = Set.contains node.State explored
+            let priority = PriorityQueue.priorityOf node frontier
+            let newPriority = problem.Heuristic node.State node.PathCost
+            match (inExplored, priority) with
+            | (false, None)                      -> PriorityQueue.add newPriority node frontier
+            | (_, Some p) when p > newPriority   -> PriorityQueue.add newPriority node frontier
+            | (_, _)                             -> frontier 
+
+        let rec aStar' frontier explored paths=
+            match PriorityQueue.tryPop frontier with
+            | None -> paths
+            | Some ((_,bestNode), frontier') -> 
+                let explored = Set.add bestNode.State explored
+                let children = childNodes problem bestNode
+                let frontier'' = List.fold (updateFrontier explored) frontier' <| children
+                aStar' frontier'' explored (bestNode::paths)
+        
+        let node = initialNode problem
+        let g = problem.Heuristic node.State node.PathCost 
+        aStar' (PriorityQueue [(g, node)]) Set.empty List.empty
+
+    let allStatesWithCost (nodes : SearchNode<_,_> list) =
+        List.map (fun n -> (n.PathCost,n.State) ) nodes
+
     let solve (solver : Problem<'s,'a,'p> -> SearchNode<'s,'a> option) problem =
         let solved = solveSearchNodePath solver problem
         match solved with
