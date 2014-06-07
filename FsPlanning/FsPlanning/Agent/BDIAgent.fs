9f311a874156921346080134431cb5bb3bdad93d
jannick johnsen <jannickboese@msn.com>
2014-06-07 11:35:33 +0200

    Begun work on fixing BDI agent

--- FsPlanning/FsPlanning/Agent/BDIAgent.fs
+++ FsPlanning/FsPlanning/Agent/BDIAgent.fs
@@ -14,6 +14,10 @@
         let mutable intentions = Map.empty
         let mutable conflicts = Map.empty
 
+        let mutable planning = Set.empty
+        let mutable onHold = Set.empty
+        let mutable executing = Set.empty
+
 
         let mutable state = initstate
         let mutable actuators = []
@@ -27,7 +31,6 @@
         //let solutionsLock = new Object()
         //let runningSolutionLock = new Object()
         //let solutionOnHoldLock = new Object()
-        let conflictLock = new Object()
         let intentionLock = new Object()
         let intentionIdLock = new Object()
         //let solvingLock = new Object()
@@ -35,35 +38,69 @@
         //Make a new ID meant for an intention
         let generateIntentionId () = lock intentionIdLock  (fun () -> intentionIdCounter <- intentionIdCounter + 1L
                                                                       intentionIdCounter)
+
+        let isIntentionOnHold id =
+            lock intentionLock (fun () -> Set.contains id onHold)
+
+        //The 3 modes intentions can be in Onhold, Planning and Executing
+        let setIntentionAsOnHold id = 
+            lock intentionLock (fun () -> 
+                onHold <- Set.add id onHold
+                planning <- Set.remove id planning
+                executing <- Set.remove id executing)
+
+        let setIntentionAsPlanning id = 
+            lock intentionLock (fun () -> 
+                onHold <- Set.remove id onHold
+                planning <- Set.add id planning
+                executing <- Set.remove id executing)
+        
+        let setIntentionAsExecuting id = 
+            lock intentionLock (fun () -> 
+                onHold <- Set.remove id onHold
+                planning <- Set.remove id planning
+                executing <- Set.add id executing)
+
+        //Removes intention from the agent
+        let removeIntention id =
+            lock intentionLock 
+                (fun () ->
+                    onHold <- Set.remove id onHold
+                    planning <- Set.remove id planning
+                    executing <- Set.remove id executing
+                    intentions <- Map.remove id intentions
+                )
+
         //Takes an intention and checks if it conflicts with any of the other intentions 
         //if it has higher desire than the other intentions then 
         let updateIntentions (intentionEqual,intenfilter) (currentInts,curConflicts) (prio,intention) =
             //printf "%A Intentions: %A\n" (List.length <| Map.toList intentions) (List.map snd <| Map.toList intentions)
             let allcurrentInts = List.map (fun (_,(_,i,_)) -> i) <| Map.toList currentInts
-            let allcurrentCons = List.map (fun (_,i) -> i) <| Map.toList curConflicts
+            let allcurrentCons = List.map (fun (_,(i,_)) -> i) <| Map.toList curConflicts
             let alreadyExists = List.exists (fun i -> intentionEqual (i, intention)) <| allcurrentCons@allcurrentInts
             if alreadyExists then
                 (currentInts,curConflicts)
             else
-                let (conflics,harmonic) = Map.partition (fun  _ (_,i,_) ->  
+                let (conflicts,harmonic) = Map.partition (fun  _ (_,i,_) ->  
                                                         let filter = intenfilter (i,intention)
                                                         match filter with
                                                         | Conflictive -> true
                                                         | Harmonic -> false
                                                         ) currentInts
-                let highestPrio = Map.forall (fun _ (cp,_,_) -> prio < cp) conflics
+                let highestPrio = Map.forall (fun cid (cp,_,_) -> prio < cp) conflicts
             
             
-                let mappedConflicts = Map.ofSeq << Seq.map (fun (_,(desire,intent,_)) -> (desire,intent)) <| Map.toSeq conflics
+                
                 if highestPrio then
+                    let mappedConflicts = Map.ofSeq << Seq.map (fun (cid,(desire,intent,_)) -> (desire,(intent, Some cid))) <| Map.toSeq conflicts
                     let id = generateIntentionId()
                     let token = new CancellationTokenSource()
-                    Map.iter (fun _ (_,_,t:CancellationTokenSource) -> t.Cancel()) conflics
+                    //Map.iter (fun _ (_,_,t:CancellationTokenSource) -> t.Cancel()) conflics
                     let newIntentions = Map.add id (prio,intention,token) harmonic
                     let newConflicts = Map.ofList ((Map.toList mappedConflicts)@(Map.toList curConflicts))
                     (newIntentions,newConflicts)
                 else
-                    (currentInts,Map.add prio intention curConflicts)
+                    (currentInts,Map.add prio (intention,None) curConflicts)
 
         
         let actionHandler act = 
@@ -105,18 +142,38 @@
                                                          state)
                 let finalplan = planner.RepairPlan(newState, intention, plan)    
                 match finalplan with
-                | Some p -> planner.NextAction (newState, intention, p)
-                | _ -> None
+                | Some p -> 
+                    match planner.NextAction (newState, intention, p) with
+                    | Some action -> Choice2Of2 action
+                    | None -> Choice1Of2 false
+                | _ -> Choice1Of2 false
             else
                 lock stateLock (fun () -> state <- planner.UpdateStateOnSolutionFinished(state,intention,plan))
-                None
+                Choice1Of2 true
         let updateConflicts newCons =
-            lock conflictLock (fun () -> conflicts <-  newCons )//Map.fold (fun cons desire inte -> Map.add desire inte cons) conflicts newCons )
+            lock intentionLock (fun () -> conflicts <-  newCons )//Map.fold (fun cons desire inte -> Map.add desire inte cons) conflicts newCons )
 
-        let updateAndStartIntentions intentionExecuter intentionFilter currentIntentions updatedIntentions =
+        let updateAndStartIntentions intentionExecuter intentionFilter currentIntentions updatedIntentions newCons =
               let (_,difIntents) = Map.partition (fun id _ -> Map.containsKey id currentIntentions) updatedIntentions
+              //let realCons = 
               lock intentionLock (fun () -> intentions <- updatedIntentions)
               Map.iter (fun id _ -> Async.Start <| intentionExecuter intentionFilter id) difIntents
+
+        
+        let rec planHandler intent (token:CancellationTokenSource) plan =
+            async
+                {
+                    if token.IsCancellationRequested then
+                        let actionAttempt = findNextAction intent plan
+                        match actionAttempt with
+                        | Choice1Of2 success -> return success
+                        | Choice2Of2 (act,rest) ->
+                            let! resolved = actionHandler act
+                            return! planHandler intent token rest
+                    else
+                        return false
+                }
+        
 
         
         let rec intentionHandler filter id =
@@ -134,35 +191,22 @@
                                                     None
                         match planAttempt with
                         | Some plan ->
-                            let curplan = ref plan
-                            let running = ref true
-                            while !running do
-                                if not token.IsCancellationRequested then
-                                    let actionAttempt = findNextAction intent !curplan
-                                    match actionAttempt with
-                                    | Some (act,rest) ->
-                                        let! resolved = actionHandler act 
-                                        if resolved then
-                                            curplan := rest
-                                        else
-                                            running := false                                   
-                                    | None -> running := false
-                                else
-                                    running:=false  
-                            
-                        | None -> ()
+                            setIntentionAsExecuting id
+                            let! success = planHandler intent token plan
+                            if not success then
+                                setIntentionAsOnHold id
+                            else
+                                removeIntention id                           
+                        | None -> 
+                            setIntentionAsOnHold id
 
                         lock intentionLock 
-                                (fun () ->   
-                                    intentions <- Map.remove id intentions
-                                    lock conflictLock
-                                        (fun () ->
-                                            let a = pintent
-                                            let (newIntents,newCons) = List.fold (updateIntentions filter) (intentions,Map.empty) <| Map.toList conflicts
-                                            updateAndStartIntentions intentionHandler filter intentions newIntents
-                                            updateConflicts newCons 
-                                            ()            
-                                        )
+                                (fun () ->
+                                    let a = pintent
+                                    let (newIntents,newCons) = List.fold (updateIntentions filter) (intentions,Map.empty) <| Map.toList conflicts
+                                    let realCons = updateAndStartIntentions intentionHandler filter intentions newIntents newCons
+                                    updateConflicts realCons
+                                    ()            
                                 )
 
                     | _ -> ()
@@ -189,8 +233,8 @@
 //                    let (_,difIntents) = Map.partition (fun id _ -> Map.containsKey id currentIntentions) updatedIntentions
 //                    intentions <- updatedIntentions
 //                    Map.iter (fun id _ -> Async.Start <| intentionHandler intentionFilter id) difIntents
-                    updateAndStartIntentions intentionHandler intentionFilter currentIntentions updatedIntentions
-                    newConflicts
+                    updateAndStartIntentions intentionHandler intentionFilter currentIntentions updatedIntentions newConflicts
+                    
                 )
             updateConflicts newCons 
         
